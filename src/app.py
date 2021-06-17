1bd770a624c9ab0ffda91c58daca98db31a1f0cd
Sudarshan <sudarshan.naidu@sts.in>
2021-06-17 21:34:48 +0530

    code updated

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello,world!"
+    return "Hello,world! welcome to python heroku world"
 
 if __name__ == "__main__":
     app.run()
